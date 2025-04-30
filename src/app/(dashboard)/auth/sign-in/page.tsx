70d17b0c0d34b23e9e830fe7f264868ef2e9cde4
AdejohOS <elusivebrown@gmail.com>
2025-04-30 08:34:50 +0100

    eslint complete

--- src/app/(dashboard)/auth/sign-in/page.tsx
+++ src/app/(dashboard)/auth/sign-in/page.tsx
@@ -1,25 +1,10 @@
 import { SignInCard } from "@/features/buyer/auth/sign-in-card";
 import { createClient } from "@/utils/supabase/server";
-import { redirect } from "next/navigation";
 
 import React from "react";
 
-type SignInPageProps = {
-  searchParams: { [key: string]: string | string[] | undefined };
-};
-
-const SignInPage = async ({ searchParams }: SignInPageProps) => {
+const SignInPage = async () => {
   const supabase = await createClient();
-
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-
-  if (user) {
-    const redirectTo =
-      typeof searchParams.redirect === "string" ? searchParams.redirect : "/";
-    redirect(redirectTo);
-  }
 
   return <SignInCard />;
 };
