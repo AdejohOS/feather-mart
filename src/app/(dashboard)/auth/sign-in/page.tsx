b4be53790fafcb3bffc8982b73dec03f4112ca23
AdejohOS <elusivebrown@gmail.com>
2025-04-30 08:37:19 +0100

    eslint complete

--- src/app/(dashboard)/auth/sign-in/page.tsx
+++ src/app/(dashboard)/auth/sign-in/page.tsx
@@ -1,11 +1,8 @@
 import { SignInCard } from "@/features/buyer/auth/sign-in-card";
-import { createClient } from "@/utils/supabase/server";
 
 import React from "react";
 
 const SignInPage = async () => {
-  const supabase = await createClient();
-
   return <SignInCard />;
 };
 
