71033eaf31a27bd11d39d690d522f38cac7acdc1
AdejohOS <elusivebrown@gmail.com>
2025-05-05 00:47:33 +0100

    mobile

--- src/app/(dashboard)/search/_components/search-bar.tsx
+++ src/app/(dashboard)/search/_components/search-bar.tsx
@@ -84,7 +84,6 @@ export const SearchBar = () => {
           <Search className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-gray-500" />
           <Input
             placeholder="Search products, farms, categories..."
-            type="search"
             className="w-full pr-10 pl-10 h-12"
             value={query}
             onChange={(e) => setQuery(e.target.value)}
